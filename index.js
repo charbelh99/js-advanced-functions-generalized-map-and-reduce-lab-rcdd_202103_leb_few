b1e6109145bd7eca866910a348936d7dceec732a
Charbel Hbayter <charbel.hbayter@lau.edu>
2021-05-03 19:13:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,18 @@ function map(obj, fn) {
   return arr
 }
 
+function reduce(obj, cb, accumulator) {
+  const keys = Object.keys(obj)
+  let i = 1
+
+  if (accumulator) {
+    i = 0
+  } else {
+    accumulator = obj[keys[0]]
+  }
+
+  for (i; i < keys.length; i++) {
+    accumulator = cb(accumulator, obj[keys[i]])
+  }
+  return accumulator
+}
