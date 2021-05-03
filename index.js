411fe35982e9587923da165adc2cb732e8465de7
Charbel Hbayter <charbel.hbayter@lau.edu>
2021-05-03 19:00:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ function map(array,){
   }
   return array;
 }
+function map(array,){
+  for(let i=0;i<array.length;i++){
+    array[i]=array[i]*2;
+  }
+  return array;
+}
+function map(array,){
+  for(let i=0;i<array.length;i++){
+    array[i]=array[i]*array[i];
+  }
+  return array;
+}
